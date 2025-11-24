050504d697066e558e0fe706975e1955ff47402b
Hatakun <user.email>
2025-11-24 19:48:17 +0900

    ./format-docker.shを実行

--- backend/app/API.py
+++ backend/app/API.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import Session
 from .models import Item
 
 from fastapi import HTTPException
+
 app = FastAPI()
 
 # CORS設定
@@ -50,15 +51,17 @@ async def get_item(db: Session = Depends(get_DB)):
 async def create_item(item: dict):
     return {"message": "Item created successfully.", "item": item}
 
-#多分いらんけど，一応確認用として
+
+# 多分いらんけど，一応確認用として
 @app.get("/api/{id}/getItem")
-async def get_item_by_id(id: int,db: Session =Depends(get_DB)):
+async def get_item_by_id(id: int, db: Session = Depends(get_DB)):
     item_by_id = db.query(Item).filter(Item.id == id).first()
     if item_by_id is None:
         raise HTTPException(status_code=404, detail="Item not found")
     return item_by_id
 
-#タグ検索用
+
+# タグ検索用
 @app.get("/api/{tag}/getItems")
 async def get_items_by_tag(tag: str, db: Session = Depends(get_DB)):
     items_by_tag = db.query(Item).filter(Item.tag == tag).all()
