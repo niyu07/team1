39d640bd0fb6e7b356c03e61da6e230852501335
池川正真 <>
2025-11-25 13:25:06 +0900

    update: models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from .database import Base
 
 
@@ -7,3 +7,9 @@ class Item(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
+    date = Column(String, index=True)
+    detail = Column(String, index=True)
+    tag = Column(String, index=True)
+    state = Column(Boolean, index=True)
+
+
