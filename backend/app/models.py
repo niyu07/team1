a3c60709f7db83793ce8dabc0c0d7c5468308afe
池川正真 <>
2025-11-25 13:29:09 +0900

    update: datetime

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,13 +1,13 @@
 from sqlalchemy import Column, Integer, String, Boolean
 from .database import Base
-
+import datetime
 
 class Item(Base):
     __tablename__ = "item"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
-    date = Column(String, index=True)
+    date = Column(String, index=True, default=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     detail = Column(String, index=True)
     tag = Column(String, index=True)
     state = Column(Boolean, index=True)
